651f6895c02d3e29486591b8c66a439a0deabd12
neonimi <suki.ao.yu@gmail.com>
2021-12-13 10:27:08 +0900

    fix todos.js

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -1,3 +1,11 @@
 import axios from "axios";
 
 const todoDataUrl = "http://localhost:3100/todos";
+
+export const fetchData = async () => {
+    // リクエスト後の値をresponseに保存
+    const response = await axios.get(todoDataUrl);
+
+    // TODOリストに現在の値としてセット
+    setTodoList(response.data);
+};
